20a7855ae8c3642ffdfdace5ac03bbb8a09a9178
Max Olifer <molu74@RD0033178.global.scd.scania.com>
2015-02-02 16:20:47 +0100

    minor fixes

--- headers/mod_main.h
+++ headers/mod_main.h
@@ -1,6 +1,8 @@
 #ifndef _modu_main_H
 #define	_modu_main_H
 
+#define ADMISSIBLE_LEVEL 1
+
 int MainModule(int coeff, int a, int b);
 
 #endif	/* _modu_a_H */
--- src/mod_main.c
+++ src/mod_main.c
@@ -1,19 +1,19 @@
 #include "../headers/mod_constr.h"
 #include "../headers/mod_adj.h"
 #include "../headers/mod_rc.h"
+#include "../headers/mod_main.h"
 
 int MainModule(int coeff, int a, int b) {
-	int coef = 10;
 	float xParam = 0;
 	int result = 0;
 
-	int safeCoef = ApplySafetyConstraints(coef);
+	int safeCoeff = ApplySafetyConstraints(coeff);
 	int mult_result = Mult(a,b);
 	int adjusted_result = AddjustUrgent(mult_result);
 
-	xParam = (float) adjusted_result / safeCoef;
+	xParam = (float) adjusted_result / safeCoeff;
 
-	if(xParam >= 1 ) {
+	if(xParam >= ADMISSIBLE_LEVEL ) {
 		result = 1;
 	} else {
 		result = 0;
